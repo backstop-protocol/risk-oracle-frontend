9f059564a1c7155191eb4898243b6b79b472e5ab
shmuel <shmuel.disraeli@gmail.com>
2023-02-21 15:07:59 +0200

    selected asset on sidenav

--- src/components/SideNavItem.js
+++ src/components/SideNavItem.js
@@ -1,16 +1,17 @@
 import mainStore from "../stores/main.store"
+import { observer } from "mobx-react"
 
-const SideNavItem = props => {
+const SideNavItem = observer(props => {
   const {asset} = props
-  const {search} = mainStore
+  const {search, selectedAsset} = mainStore
   return (
     <li className="side-nav-item">
-      <a className="secondary" onClick={()=>search(asset.name)}>
+      <a className={`${selectedAsset && asset.name === selectedAsset.name ? 'selected' : ''} secondary`} onClick={()=>search(asset.name)}>
         <img alt={`${asset.name} icon`} src={`/asset-icons/${asset.name}.webp`}/>  
         <span>{asset.name}</span>
       </a>
     </li>
   )
-}
+})
 
 export default SideNavItem
