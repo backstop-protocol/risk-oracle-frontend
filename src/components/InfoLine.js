d1a527d8666545b83149b3fe0208a8d752244c3c
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-03-30 18:08:08 +0200

    infoline now plugged into the files

--- src/components/InfoLine.js
+++ src/components/InfoLine.js
@@ -1,7 +1,27 @@
-import { observer } from "mobx-react"
+import assetsDataStore from '../stores/assets.data.store';
+import mainStore from '../stores/main.store';
+import { observer } from "mobx-react";
 
 const InfoLine = observer(props => {
   const {dataStore} = props
+  const selectedBase = mainStore.selectedAsset;
+  const selectedBaseSymbol = selectedBase.name === 'ETH' ? 'WETH' : selectedBase.name;
+  const data = assetsDataStore.data;
+  const dataForPriceComputation = data['uniswapv2'][1];
+  const priceInfo = {};
+  if(dataForPriceComputation){
+    const dataForBase = dataForPriceComputation.filter(_ => _.base.toLowerCase() === selectedBaseSymbol.toLowerCase());
+    for(let i = 0; i < dataForBase.length; i++){
+      if(dataForBase[i].quote === 'USDC'){
+        priceInfo['startPrice'] = dataForBase[i].startPrice.toFixed(2);
+        priceInfo['lastPrice'] = dataForBase[i].endPrice.toFixed(2);
+        priceInfo['startLiquidity'] = dataForBase[i].volumeForSlippage[0][1].toFixed(2);
+        priceInfo['endLiquidity'] = dataForBase[i].volumeForSlippage.slice(-1)[0][1].toFixed(2);
+      }
+    }
+    console.log(priceInfo);
+  }
+
   return (
     <div className="info-line">
       <div className="info">
@@ -10,17 +30,17 @@ const InfoLine = observer(props => {
         </span>
         <small>
           <span>
-            price: <strong>${dataStore.price}</strong>
+            price: <strong>${priceInfo.lastPrice}</strong>
           </span>
         </small>
         <small>
           <span>
-            24H price change: <strong>{dataStore._24PriceChange}%</strong>
+            24H price change: <strong>{((Number(priceInfo.lastPrice) - Number(priceInfo.startPrice)) / Number(priceInfo.startPrice) * 100).toFixed(2)}%</strong>
           </span>
         </small>
         <small>
           <span>
-            24H Liquidity change: <strong>2.7%</strong>
+            24H Liquidity change: <strong>{((Number(priceInfo.endLiquidity) - Number(priceInfo.startLiquidity)) / Number(priceInfo.endLiquidity) * 100).toFixed(2)}%</strong>
           </span>
         </small>
       </div>
