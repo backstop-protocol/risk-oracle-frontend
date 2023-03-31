3617cf9cdba65b03fde14c65be67966654e845bf
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-03-31 14:24:37 +0200

    fixed util to display large numbers

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,7 +4,20 @@
  * @param {number} dec how many decimals
  * @returns 
  */
-export default function roundTo(num, dec = 2) {
+export function roundTo(num, dec = 2) {
     const pow = Math.pow(10, dec);
     return Math.round((num + Number.EPSILON) * pow) / pow;
+}
+
+export function largeNumberFormatter(number){
+    if(number >= 1e9){
+        return `${(Number((number/(1e9)).toFixed(2)))}B`;
+    } 
+    if(number >= 1e6){
+        return `${(Number((number/(1e6)).toFixed(2)))}M`
+    } 
+    if(number >= 1e3){
+        return `${(Number((number/(1e3)).toFixed(2)))}K`
+    }
+    return number
 }
