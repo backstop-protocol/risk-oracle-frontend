7d515fe267736352fba3b3592a629e7d143c73ae
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-03-31 11:17:27 +0200

    Dex selector now working

--- src/components/DexSelector.js
+++ src/components/DexSelector.js
@@ -1,13 +1,23 @@
 import assetsDataStore from "../stores/assets.data.store";
+import mainStore from "../stores/main.store";
 import { observer } from "mobx-react";
 
 const nameMap = {
-  uniswapv2: "Uniswap v2"
+  uniswapv2: "Uniswap v2",
+  curve: 'Curve'
+};
+
+function isDexAvailableForBase(dexName, selectedBaseName){
+  const availableBases = assetsDataStore.data[dexName]['1'].map(_ => _.base);
+  return availableBases.includes(selectedBaseName);
 }
 
 const DexSelector = observer(props => {
   const selectedDexes = props.dexes;
   const availableDexes = assetsDataStore.platforms;
+  const selectedBase = mainStore.selectedAsset;
+  const selectedBaseName = selectedBase.name === 'ETH' ? 'WETH' : selectedBase.name;
+
   return (
     <div style={{fontSize: "0.875em"}}>
       <fieldset className="dex-selector">
@@ -18,7 +28,7 @@ const DexSelector = observer(props => {
           all dexs
         </label> */}
         {availableDexes.map(dex=> <label key={dex} htmlFor={dex}>
-          <input type="checkbox" id={dex} name={dex} checked={selectedDexes.includes(dex)} onChange={()=> props.handleChange(dex)}/>
+          <input type="checkbox" id={dex} name={dex} checked={selectedDexes.includes(dex)} disabled={!isDexAvailableForBase(dex, selectedBaseName)} onChange={()=> props.handleChange(dex)}/>
           {nameMap[dex] || dex}
         </label>)}
       </fieldset>
