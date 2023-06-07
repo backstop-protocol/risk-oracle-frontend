619051c6a4c9fb8e2791137810c3cf14b6bf04a7
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-06-07 16:23:59 +0200

    only allow numbers

--- src/components/LTVCalculator.js
+++ src/components/LTVCalculator.js
@@ -6,11 +6,10 @@ import { useState } from "react";
 const LTVCalculator = observer(props => {
     const quotes = props.quotes;
     const [selectedQuote, setSelectedQuote] = useState(quotes[0]);
+    const [borrowCap, setBorrowCap] = useState(0);
     const span = mainStore.selectedSpan;
     const slippage = mainStore.selectedSlippage;
-    const slippageOptions = [
-        1, 5, 10, 15, 20
-    ];
+    const slippageOptions = [1, 5, 10, 15, 20];
 
 
     return (
@@ -34,7 +33,7 @@ const LTVCalculator = observer(props => {
                     <td>a</td>
                     <td>l</td>
                     <td><select value={slippage} onChange={(event) => { mainStore.handleSlippageChange(event.target.value) }}>{slippageOptions.map((_) => <option key={_} value={_}>{_}</option>)}</select></td>
-                    <td></td>
+                    <td><input type="tel" value={borrowCap} onChange={(event)=> {setBorrowCap(((event.target.value || '').match(/^[0-9]+(\.[0-9]{0,2})?/g) || [])[0] || '')}} /></td>
                 </tbody>
             </table>
         </article>
