9704cf38867de9531f04932d7c0c2e569ef09c0d
Edouard VIGUIER <33686588+0xVorian@users.noreply.github.com>
2023-07-08 11:57:41 +0200

    cleanup of web3data module

--- src/components/Web3Data.js
+++ src/components/Web3Data.js
@@ -10,33 +10,31 @@ import { pythiaAddress } from "../config";
 const Web3Data = observer(props => {
     const web3Data = mainStore.web3Data;
     const span = mainStore.selectedSpan;
-    if (!web3Data) {
-        return <Skeleton variant="rectangular" height={'5vh'} width={'12vw'} />
-    }
-    {
-        const selectedBase = mainStore.selectedAsset.name;
-        if (web3Data[selectedBase]) {
-            return (
-                <article className="web3data" style={{ display: 'flex', justifyContent: 'space-between', alignItems: "start", margin:"0 !important" }}>
-                    <div >
-                        <InfoLine />
-                        <ContractAddress address={pythiaAddress} />
-                    </div>
-                    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100%', alignItems: "end", alignContent: 'end', flexWrap: 'wrap' }}>
-                        <div style={{ minHeight: '50%' }}>
-                            Avg 30 days uniV3 liquidity vs USDC
-                            <br />
+    const selectedBase = mainStore.selectedAsset.name;
+    return (
+        <article className="web3data" style={{ display: 'flex', justifyContent: 'space-between', alignItems: "start", margin: "0 !important" }}>
+            <div >
+                <InfoLine />
+                <ContractAddress address={pythiaAddress} />
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100%', alignItems: "end", alignContent: 'end', flexWrap: 'wrap' }}>
+                <div style={{ minHeight: '50%' }}>
+                    Avg 30 days uniV3 liquidity vs USDC
+                    <br />
+                    {web3Data && web3Data[selectedBase] ?
+                        <p>
                             {selectedBase}: {largeNumberFormatter(web3Data[selectedBase]['value'])} (${largeNumberFormatter(web3Data[selectedBase]['value'] * mainStore.basePrice)})
-                        </div>
-                    </div>
-                    <div>
-                        <LastUpdate date={mainStore.lastUpdate[span]} />
-                    </div>
-                </article>
+                        </p>
+                        : <Skeleton />}
+                </div>
+            </div>
+            <div>
+                <LastUpdate date={mainStore.lastUpdate[span]} />
+            </div>
+        </article>
 
-            )
-        }
-    }
+    )
 })
+
 
 export default Web3Data
