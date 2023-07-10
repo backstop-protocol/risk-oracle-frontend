8b46b3039d684a3d6e3211f061ea7bdb04d93759
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-07-10 14:43:50 +0200

    cosmetic changes for sidenav

--- src/sections/SideNav.js
+++ src/sections/SideNav.js
@@ -1,4 +1,4 @@
-import { Box, Tab, Tabs } from "@mui/material";
+import { Box, Paper, Tab, Tabs } from "@mui/material";
 
 import mainStore from "../stores/main.store";
 import { observer } from "mobx-react";
@@ -8,15 +8,17 @@ const SideNav = observer(props => {
   function handleClick(e, v){
     search(v);
   }
-  return (<Box sx={{ width: "7vw", marginTop: "8vh", height: "100vh", position: "sticky", position: "-webkit-sticky", top: 0, left: 0 }}>
+  return (<Paper sx={{ width: "7vw", marginTop: "8vh", height: "100vh", position: "sticky", position: "-webkit-sticky", top: 0, left: 0 }}>
     <Tabs
       orientation="vertical"
       value={selectedBaseSymbol === "WETH" ? "ETH" : selectedBaseSymbol}
       onChange={handleClick}
+      textColor="text.primary"
+      indicatorColor="secondary"
     >
-      {assets.map(_ => <Tab value={_.name} iconPosition="start" color="text.primary" icon={<Box component="img" sx={{height:"3vh"}} alt={`${_.name} icon`} src={`/asset-icons/${_.name}.webp`} />} label={_.name} />)}
+      {assets.map(_ => <Tab value={_.name} iconPosition="start"  icon={<Box component="img" sx={{height:"3vh"}} alt={`${_.name} icon`} src={`/asset-icons/${_.name}.webp`} />} label={_.name} />)}
     </Tabs>
-  </Box>
+  </Paper>
   )
 })
 
