74643c8f12f765aa8969141dcfde9158d148b2cb
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-07-14 13:50:00 +0200

    changed twitter logo

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -1,7 +1,8 @@
 import { Box, Link, Stack, Typography } from "@mui/material";
 import mainStore from "../stores/main.store";
+import { observer } from "mobx-react";
 
-const Footer = props => {
+const Footer = observer(props => {
   return <Box sx={{height:"15vh",display: 'flex', flexDirection: 'column', alignItems:"center", justifyContent:"center", width: '100%' }}>
     <Box sx={{display: 'flex', flexDirection: 'row', marginBottom:"1vh"}}>
       <img className="footer-logo" alt="" src="logo.svg" />
@@ -15,10 +16,10 @@ const Footer = props => {
     <Stack sx={{padding:"1vh 0 1vh 0"}} gap={4} direction="horizontal">
       <Link className="footer-social-link" href="https://github.com/backstop-protocol"><img alt="Github Logo" src={mainStore.darkTheme ? "images/social/github-mark-white.svg" : "images/social/github-mark.svg"} /></Link>
       <Link className="footer-social-link" href="https://discord.com/invite/bJ4guuw"><img alt="Discord Logo" src={mainStore.darkTheme ? "images/social/discord-mark-white.svg" : "images/social/discord-mark-black.svg"} /></Link>
-      <Link className="footer-social-link" href="https://twitter.com/bprotocoleth"><img alt="Twitter Logo" src={mainStore.darkTheme ? "images/social/twitter-white.svg" : "images/social/twitter-blue.svg"} /></Link>
+      <Link className="footer-social-link" href="https://twitter.com/bprotocoleth"><img alt="Twitter Logo" src={mainStore.darkTheme ? "images/social/twitter-white.svg" : "images/social/twitter-black.svg"} /></Link>
       <Link className="footer-social-link" href="https://medium.com/b-protocol"><img  alt="Medium Logo" src={mainStore.darkTheme ? "images/social/medium-white.png" : "images/social/medium-black.png"} /></Link>
       </Stack>
   </Box>
-}
+})
 
 export default Footer
