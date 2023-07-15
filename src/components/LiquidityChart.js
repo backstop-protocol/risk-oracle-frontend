106755ffb2db0c29bfd4a000b1d96e074d054781
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-07-15 17:21:32 +0200

    Update LiquidityChart.js

--- src/components/LiquidityChart.js
+++ src/components/LiquidityChart.js
@@ -45,7 +45,7 @@ const CustomTooltip = ({ active, payload, label }) => {
 const LiquidityChart = observer(props => {
  const {quotes, displayData, selectedBaseSymbol} = props;
   return (
-    <Paper sx={{display:"flex", flexDirection:"column", minWidth:"60%", maxWidth:"80%", height:"50%", minHeight: '410px', marginRight: "1vw", marginLeft:"1vw" }}>
+    <Paper sx={{display:"flex", flexDirection:"column", minWidth:"50%", maxWidth:"80%", height:"50%", minHeight: '410px', marginRight: "1vw", marginLeft:"1vw" }}>
       <Box sx={{padding:"1vh 1vw 0 1vw"}}>
       <GraphControls selectedBaseSymbol={props.selectedBaseSymbol} availableQuotesForBase={props.availableQuotesForBase} />
       </Box>
