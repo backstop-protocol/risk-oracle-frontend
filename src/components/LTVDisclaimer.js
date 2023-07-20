426fe4250b2773f12f9ab860818f73c9061b15d8
Edouard VIGUIER <33686588+0xVorian@users.noreply.github.com>
2023-07-20 14:53:55 +0200

    final commit for mobile version

--- src/components/LTVDisclaimer.js
+++ src/components/LTVDisclaimer.js
@@ -1,10 +1,11 @@
-import { Box, Typography, Link } from "@mui/material";
+import { Box, Typography, Link, Divider } from "@mui/material";
 
 export default function LTVDisclaimer(){
     return(
         <Box sx={{width:"90%", flex:"0 2 auto"}}>
         <small fontWeight="bold">*Disclaimer: The Smart LTV formula is experimental and uses testnet data. It is for research purposes only and at this stage may lead to risk management flaws and to severe financial losses. </small>
         <small fontSize="0.875em" fontWeight="bold">Learn more about the risks involved <Link target="_blank" href="https://app.gitbook.com/o/-MdGrvWrIrA1v6TtCihZ/s/-MdGr7n_D-cGTaeor0FV/~/changes/50/technical-documentation/smart-ltv-formula" color="#3366CC">here.</Link></small>
+  <Divider sx={{width:"100%", margin:"5% 0 5% 0"}} />
         </Box>)
 }
 
--- src/sections/GraphPanel.js
+++ src/sections/GraphPanel.js
@@ -73,8 +73,7 @@ const MainPanel = observer(props => {
     </div>)
   }
   return (
-      <Box sx={{flex:"0 2 auto", marginTop:"5%", width:"100%", height:"100%", display:'flex', flexDirection:"column", justifyContent:"space-evenly", alignItems:"center", rowGap:"3%"}}>
-  <Divider sx={{width:"100%", marginBottom:"5%"}} />
+      <Box sx={{flex:"0 2 auto", marginTop:"5%", width:"100%", height:"100%", display:'flex', flexDirection:"column", justifyContent:"space-evenly", alignItems:"center", rowGap:"7%"}}>
       <Box sx={{display:'flex', width:"100%", flexDirection:"row", justifyContent:"space-around", alignItems:"center", rowGap:"2%",flexWrap:"wrap"}}>
         {mobile ? 
         <LiquidityChartMobile availableQuotesForBase={availableQuotesForBase} selectedBaseSymbol={selectedBaseSymbol} quotes={quotes} loading={loading} displayData={displayData} dataStore={dataStore} />
